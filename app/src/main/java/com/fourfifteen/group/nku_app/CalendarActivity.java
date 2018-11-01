1e4b4975e4c3ba38d847f20f5f850d8095cfac0c
garrettfoister <garrettscottfoister@gmail.com>
2018-11-01 03:05:41 -0400

    Update app/src/main/java/com/fourfifteen/group/nku_app/CalendarActivity.java
    Changed formatting

--- app/src/main/java/com/fourfifteen/group/nku_app/CalendarActivity.java
+++ app/src/main/java/com/fourfifteen/group/nku_app/CalendarActivity.java
@@ -24,9 +24,9 @@ public class CalendarActivity extends AppCompatActivity {
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
-                currentDate.setText("Date: " + i2 + " / " + i1 + " / " + i);
+                currentDate.setText("Date: " + i1 + " / " + i2 + " / " + i);
 
-                Toast.makeText(getApplicationContext(), "Selected Date:\n" + "Day = " + i2 + "\n" + "Month = " + i1 + "\n" + "Year = " + i, Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Selected Date:\n" + "Month = " + i1 + "\n" + "Day = " + i2 + "\n" + "Year = " + i, Toast.LENGTH_LONG).show();
             }
         });
     }
