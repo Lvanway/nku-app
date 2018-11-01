963c80a71e9e0fea2dcf14c8bfd09d4f615b608c
garrettfoister <garrettscottfoister@gmail.com>
2018-11-01 02:58:26 -0400

    Added WIP calendar activity

---
+++ app/src/main/java/com/fourfifteen/group/nku_app/CalendarActivity.java
@@ -0,0 +1,33 @@
+package com.fourfifteen.group.nku_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    CalendarView calendar;
+    TextView currentDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+
+        calendar = (CalendarView) findViewById(R.id.calendar);
+        currentDate = (TextView) findViewById(R.id.currentDate);
+        currentDate.setText("Date: ");
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+                currentDate.setText("Date: " + i2 + " / " + i1 + " / " + i);
+
+                Toast.makeText(getApplicationContext(), "Selected Date:\n" + "Day = " + i2 + "\n" + "Month = " + i1 + "\n" + "Year = " + i, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
--- app/src/main/java/com/fourfifteen/group/nku_app/MainActivity.java
+++ app/src/main/java/com/fourfifteen/group/nku_app/MainActivity.java
@@ -1,13 +1,32 @@
 package com.fourfifteen.group.nku_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button calendarButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendarButton = (Button) findViewById(R.id.calendarButton);
+
+        // Capture calendarButton clicks
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        CalendarActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
     }
 }
