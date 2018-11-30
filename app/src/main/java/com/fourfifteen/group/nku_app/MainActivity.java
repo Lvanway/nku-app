74f58580cdb1b7451ae1828f7c7b82c0a2fafff9
James Kennedy <James_Kennedy87@yahoo.com>
2018-11-29 19:27:38 -0500

    Updated Main Image Attribute
    Fixed scaling issue.

--- app/src/main/java/com/fourfifteen/group/nku_app/MainActivity.java
+++ app/src/main/java/com/fourfifteen/group/nku_app/MainActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.ImageViewCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
